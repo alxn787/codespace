0fa0ff787c526ad0100c3350dbe030f234adddab
alxn787 <alenabraham787@gmail.com>
2025-07-04 12:19:43 +0530

    playground routes

--- apps/server/routes/playground-routes.ts
+++ apps/server/routes/playground-routes.ts
@@ -8,40 +8,94 @@ const
 router  = Router();
 
 router.post("/playground", authMiddleware, async (req, res) => {
-  const { name, environment, port } = req.body;
-  //@ts-ignore
-  const decodedUser = req.decodedUser;
-  const userId = decodedUser.userId;
-  let template = environment;
-  let containerImage = "";
-  let containerPort = 5001;
-  if (template === "reactjs") {
-    containerImage = "alxn787/react-codespace:2";
-    containerPort = 5173;
-  } else if (template === "nodejs") {
-    containerImage = "alxn787/node-codespace:2";
-    containerPort = 5001;
-  }
-  console.log(containerImage, containerPort,decodedUser);
-
-  const codespace = await prisma.playground.create({
-    data: {
-      title: name,
-      template: environment,
-      containerPort: containerPort,
-      containerImage: "alxn787/react-codespace:2",
-      user: {
-          connect: {
-            id: userId,
+  try{ 
+    const { name, environment } = req.body;
+    //@ts-ignore
+    const decodedUser = req.decodedUser;
+    const userId = decodedUser.userId;
+    let template = environment;
+    let containerImage = "";
+    let containerPort = 5001;
+    if (template === "reactjs") {
+      containerImage = "alxn787/react-codespace:2";
+      containerPort = 5173;
+    } else if (template === "nodejs") {
+      containerImage = "alxn787/node-codespace:2";
+      containerPort = 5001;
+    }
+    console.log(containerImage, containerPort,decodedUser);
+  
+    await PublishMessage(name, environment, containerPort);
+  
+    const codespace = await prisma.playground.create({
+      data: {
+        title: name,
+        template: environment,
+        containerPort: containerPort,
+        containerImage: "alxn787/react-codespace:2",
+        user: {
+            connect: {
+              id: userId,
+            },
           },
-        },
-    },
-  });
-  await PublishMessage(name, environment, port);
+      },
+    });
+    res.status(201).json({ message: "Playground created" ,
+      codespace
+    });
+  }catch(err){
+    res.status(500).json({ message: err });
+    return;
+  }
+});
 
-  res.status(201).json({ message: "Playground created" ,
-    codespace
-  });
+router.get("/playgrounds", authMiddleware, async (req, res) => {
+  try {
+    //@ts-ignore
+    const { userId } = req.decodedUser.userId;
+
+    const codespaces = await prisma.playground.findMany({
+      where: {
+        userId: userId,
+      },
+      include: {
+        user: true,
+      },
+    });
+    res.status(200).json({ message: "Playgrounds retrieved", codespaces });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+});
+
+
+router.post("/playgrounds/:id", authMiddleware, async (req, res) => {
+  try {
+    //@ts-ignore
+    const { userId } = req.decodedUser;
+
+    const { id } = req.params;
+
+    const codespace = await prisma.playground.findFirst({where:{id:id}});
+    if(!codespace){
+      res.status(404).json({ message: "Playground not found" });
+      return;
+    }
+
+    if(codespace.userId !== userId){
+      res.status(403).json({ message: "You are not authorized to delete this playground" });
+      return;
+    }
+    await prisma.playground.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    res.status(200).json({ message: "Playground deleted", codespace });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 });
 
 export default router;
